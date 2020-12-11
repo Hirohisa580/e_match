14d01e15434a40daa925b205997204edbd30cd4c
Hirohisa Koyama <n88.koyama.hirohisa@gmail.com>
2020-12-11 17:25:56 +0900

    boardモデルの単体テストコードを記載

--- spec/factories/boards.rb
+++ spec/factories/boards.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :board do
-    
+    name                  {Faker::Lorem.sentence}
+    genre_one_id          {2}
+    explanation           {Faker::Lorem.sentence}
+    association :user
+    association :profile
   end
 end
