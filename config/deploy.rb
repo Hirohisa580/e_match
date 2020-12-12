e98d001cfbd8d10f8fcb23c102260a27c933e51e
Hirohisa Koyama <n88.koyama.hirohisa@gmail.com>
2020-12-13 01:55:13 +0900

    ディレクトリ名を変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ lock '3.14.1'
 set :application, 'e_match'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:Hirohisa580/e_match_clone.git'
+set :repo_url,  'git@github.com:Hirohisa580/e_match.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
