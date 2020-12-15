4b610b5f8c5abe87744735a193c74320e2b36685
Hirohisa Koyama <n88.koyama.hirohisa@gmail.com>
2020-12-15 14:16:38 +0900

    dmscontrollerの記述を修正。それに伴ってprofileのshowビューも修正。また、検索の挙動と普段のトップページの表示を微調整

--- app/controllers/dms_controller.rb
+++ app/controllers/dms_controller.rb
@@ -5,22 +5,27 @@ class DmsController < ApplicationController
   end
 
   def create
-    binding.pry
-    dm = Dm.new(dm_params)
-    dm.save
-    profile_user_id = dm.user_ids[0]                         #選んだユーザーのユーザーidを取得
-    profile_user_dm = UserDm.where(user_id: profile_user_id)  #選んだユーザーのuser_dmsレコード情報を取得
-    profile_dm_ids = profile_user_dm.pluck(:dm_id)           #選んだユーザーが持ってるdm_idを配列で取得
-    user_dm= UserDm.where(user_id: current_user.id)           #現ユーザーのuser_dmsレコード情報を取得
-    dm_ids = user_dm.pluck(:dm_id)                           #現ユーザーが持ってるdm_idを配列で取得
-    @intersection = dm_ids & profile_dm_ids                #現ユーザーと選んだユーザーが所属しているdm_id
-    redirect_to dm_messages_path(@intersection)
+
+    ActiveRecord::Base.transaction do
+      dm = Dm.create!
+      dm.user_dms.create!(user_id: current_user.id)
+      dm.user_dms.create!(user_id: params[:user_id])
+
+      profile_user_id = params[:user_id]                         #選んだユーザーのユーザーidを取得
+      profile_user_dm = UserDm.where(user_id: profile_user_id)  #選んだユーザーのuser_dmsレコード情報を取得
+      profile_dm_ids = profile_user_dm.pluck(:dm_id)           #選んだユーザーが持ってるdm_idを配列で取得
+      user_dm= UserDm.where(user_id: current_user.id)           #現ユーザーのuser_dmsレコード情報を取得
+      dm_ids = user_dm.pluck(:dm_id)                           #現ユーザーが持ってるdm_idを配列で取得
+      @intersection = dm_ids & profile_dm_ids                #現ユーザーと選んだユーザーが所属しているdm_id
+      redirect_to dm_messages_path(@intersection)
+    end
+
   end
 
   private
 
-    def dm_params
-      params.require(:dm).permit(user_ids: [])
-    end
+    # def dm_params
+    #   params.require(:dm).permit(user_ids: [])
+    # end
     
 end
