47ed6f41adeb8c905875650b97851f091b44018c
Hirohisa Koyama <n88.koyama.hirohisa@gmail.com>
2020-12-22 20:48:54 +0900

    修正: profileが消えたらmessageも消える様に修正

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,6 @@
 class Profile < ApplicationRecord
   belongs_to :user
-  has_many :messages
+  has_many :messages, dependent: :destroy
   has_many :boards, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :schedules, dependent: :destroy
